fa6b6183f7243339e08064b9292e1453603ddbd3
Gavin Carew <gjcarew@gmail.com>
2023-03-20 16:20:41 -0700

    Output all lists

--- src/mailgunDatapoints.ts
+++ src/mailgunDatapoints.ts
@@ -9,6 +9,18 @@ import { AccessResponse, IntegrationDatapoints, SeedInput } from './types';
 // The API key
 export const MAILGUN_API_KEY = 'FILL IN FROM ACCOUNT';
 
+// The API key should not be hard-coded here, but rather taken from an environment variable
+ 
+// The base instance to build API calls
+export const mailgunInstance = axios.create({
+    baseURL: 'https://api.mailgun.net/',
+    timeout: 1000,
+    auth: {
+        username: 'api',
+        password: MAILGUN_API_KEY
+      }
+  });
+
 export const mailgunDataPoints: IntegrationDatapoints = {
   /**
    * Create mailing lists and Seed user(s) onto them
@@ -19,8 +31,29 @@ export const mailgunDataPoints: IntegrationDatapoints = {
   /**
    * Get all mailing lists that the user belongs to
    */
+
+  /**
+   * TODO: 
+   * Get the lists JSON from Mailgun API
+   * Iterate through the json to find the member
+   * Add that email list address to the list of email lists
+   * 
+   */
   access: async (identifier: string): Promise<AccessResponse> => {
-    throw new Error('Access not implemented!');
+    try {
+        const all_lists: AccessResponse = await mailgunInstance({
+            method: 'GET',
+            url: '/v3/lists/pages',
+            params: {
+                limit: 100
+              }
+        })
+        return all_lists
+      } catch (error) {
+        console.error(error);
+      }
+      
+    // throw new Error('Access not implemented!');
   },
   /**
    * Remove the user from all mailing lists.
