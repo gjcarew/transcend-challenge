a8f219232791e3042e9eaf020a4d0fb9b782ca37
Gavin Carew <gjcarew@gmail.com>
2023-03-22 09:17:16 -0700

    move getLists function outside of access

--- src/mailgunDatapoints.ts
+++ src/mailgunDatapoints.ts
@@ -38,7 +38,9 @@ export const mailgunDataPoints: IntegrationDatapoints = {
             }
         });
     } catch (error) {
-        throw(error);
+        // Expand on log error - where it happened, possible causes, time 
+        console.error('Error with seedInput')
+        throw (error);
     }
   },
   /**
@@ -46,102 +48,105 @@ export const mailgunDataPoints: IntegrationDatapoints = {
    */
 
   access: async (identifier: string): Promise<AccessResponse> => {
-    try {
-        // Define an array for all mailing list addresses for this company
-        const addressList: string[] = [];
-        
-        // Define an array for mailing lists that include the target user
-        const addressesWithUser: string[] = [];
-
-        // Set the starting URL for mailing lists
-        const url = '/v3/lists/pages';
-
-        // Function to get all mailing lists for the organization
-        const getLists = async (url) => {
-            const allMailingLists = await mailgunClient({
-                method: 'GET',
-                url: url,
-                params: {
-                    limit: 100
+      try {
+          // Define an array for all mailing list addresses for this company
+          
+          // Define an array for mailing lists that include the target user
+          const addressesWithUser: string[] = [];
+          
+          // Set the starting URL for mailing lists
+          const url = '/v3/lists/pages';
+          
+          // Function to get all mailing lists for the organization
+          
+          const addressList = await getLists(url);
+          
+          // Call each address to find if the target is a member
+          
+          for (let i = 0; i < addressList.length; i++) {
+                const address = addressList[i]
+                const response = await mailgunClient({
+                    method: 'GET',
+                    url: `/v3/lists/${address}/members/${identifier}`,
+                    // I want to continue the execution of this function with a 404 response
+                    validateStatus: function (status) {
+                        return status === 404 || status === 200;
+                    }
+                });
+                /**
+                 * This method allows the lookup to take place server-side.
+                 * This should be faster than requesting all members and looping through to find the target member.
+                 * It also prevents us from needing pagination for this section.
+                 * 
+                 * I don't like how many calls need to be made here - it works with the mocked data
+                 * but there is a good chance this would run into a rate limit errors in production. 
+                 * Those errors could be addressed with exponential backoff using a package like axios-retry
+                */
+                
+                if (response.status === 200) {
+                    addressesWithUser.push(address)
                 }
-            });
-
-            // Extract the mailing list addresses if a list has members
-            allMailingLists.data["items"].forEach((item) => {
-                if (item["members_count"] > 0) {
-                    addressList.push(item["address"])
-                }
-            });
-
-            // For pagination - recursively calls the getLists function 
-            const num_items = allMailingLists.data["items"].length
-            if (num_items === 100) {
-                const next_url = allMailingLists.data["paging"]["next"];
-                // Trim the base url from the full url for the next page to pass to getLists function
-                const trimmed_next_url =  'v3/lists/pages?'.concat(next_url.substring(49, next_url.length - 10));
-                getLists(trimmed_next_url);
             };
-        };
 
-        await getLists(url);
-
-        // Call each address to find if the target is a member
-
-        for (let i = 0; i < addressList.length; i++) {
-            const address = addressList[i]
-            const response = await mailgunClient({
-                method: 'GET',
-                url: `/v3/lists/${address}/members/${identifier}`,
-                // I want to continue the execution of this function with a 404 response
-                validateStatus: function (status) {
-                    return status === 404 || status === 200;
+            return {
+                data: addressesWithUser,
+                contextDict: {
+                    mailingLists: addressesWithUser
                 }
-            });
-            /**
-             * This method allows the lookup to take place server-side.
-             * This should be faster than requesting all members and looping through to find the target member.
-             * It also prevents us from needing pagination for this section.
-             * 
-             * I don't like how many calls need to be made here - it works with the mocked data
-             * but there is a good chance this would run into a rate limit errors in production. 
-             * Those errors could be addressed with exponential backoff using a package like axios-retry
-             */
-
-            if (response.status === 200) {
-                addressesWithUser.push(address)
+            };
+            } catch (error) {
+                // More error logging (see seed)
+                throw(error);
             }
-        };
-
-        const return_value = {
-            data: addressesWithUser,
-            contextDict: {
-                mailingLists: addressesWithUser
+        },
+        /**
+         * Remove the user from all mailing lists.
+         * NOTE: Erasure runs an Access (access()) before it to
+         * fetch the context data it might need.
+        */
+        erasure: async (identifier: string, contextDict?: object): Promise<void> => {
+           try {
+               const mailingLists = contextDict["mailingLists"]
+               for (let i = 0; i < mailingLists.length; i++) {
+                   const address = mailingLists[i]
+                   await mailgunClient({
+                       method: 'DELETE',
+                       url: `/v3/lists/${address}/members/${identifier}`,
+                    });
+                }; 
+            } catch (error) {
+                // More error logging (see seed)
+                throw (error);
             }
-        };
+        },
+    };
+    
+async function getLists (url: string): Promise<string[]> {
+    const allMailingLists = await mailgunClient({
+        method: 'GET',
+        url: url,
+        params: {
+            limit: 100
+        }
+    });
 
-        return return_value;
+    const addressList: string[] = [];
 
-    } catch (error) {
-    throw(error);
-    }
-  },
-  /**
-   * Remove the user from all mailing lists.
-   * NOTE: Erasure runs an Access (access()) before it to
-   * fetch the context data it might need.
-   */
-  erasure: async (identifier: string, contextDict?: object): Promise<void> => {
-    try {
-        const mailingLists = contextDict["mailingLists"]
-        for (let i = 0; i < mailingLists.length; i++) {
-            const address = mailingLists[i]
-            await mailgunClient({
-                method: 'DELETE',
-                url: `/v3/lists/${address}/members/${identifier}`,
-            });
-        }; 
-    } catch (error) {
-    throw (error);
-    }
-  },
+    // Extract the mailing list addresses if a list has members
+    allMailingLists.data["items"].forEach((item) => {
+        if (item["members_count"] > 0) {
+            addressList.push(item["address"])
+        }
+    });
+    
+    // For pagination - recursively calls the getLists function 
+    const num_items = allMailingLists.data["items"].length
+    if (num_items === 100) {
+        const next_url = allMailingLists.data["paging"]["next"];
+        // Trim the base url from the full url for the next page to pass to getLists function
+        const trimmed_next_url =  'v3/lists/pages?'.concat(next_url.substring(49, next_url.length - 10));
+        addressList.concat(await getLists(trimmed_next_url));
+    };
+
+    return addressList;
 };
